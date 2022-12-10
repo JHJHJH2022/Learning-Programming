0c31ca3e5e03e5871d6aabc106653c382d7755aa
Jinghan0505 <jinghanjinghan233@gmail.com>
2022-12-10 17:51:35 +0800

    Update 20221210LinkedList.js

--- Data-Structure-And-Algorithms/learning-DSA/20221210LinkedList.js
+++ Data-Structure-And-Algorithms/learning-DSA/20221210LinkedList.js
@@ -1,3 +1,5 @@
+// Following this FCC tutorial: https://www.youtube.com/watch?v=Hj_rA0dhr2I&t=1899s&ab_channel=freeCodeCamp.org
+
 //#region Constructing a linked list
 class Node{
     constructor(val){
@@ -47,7 +49,7 @@ c.next = d;
 
 
 
-//#region Practice Question: linked list values
+//#region Practice Question1: linked list values
 // Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
 
 
@@ -61,20 +63,45 @@ c.next = d;
 //     }
 //     return values;
 // }
-// console.log(LinkedListValues(a));
 
 
 // approach 2: recursive approach
 const LinkedListValues = (head) =>{
     const values = [];
     fillValues(head, values);
+    return values;
 };
-console.log(LinkedListValues(a));
 
-const fillValues(head, values)=>{
+const fillValues = (head, values)=>{
     if (head == null) return;
-    
+    values.push(head.val);
+    fillValues(head.next,values);
 };
+
+//#endregion
+
+//#region Practice Question2: Sum List
+
+// 1. iterative approach
+// const sumList = (head) =>{
+//     let sum = 0;
+//     let current = head;
+//     while (current != null){
+//         sum += current.val;
+//         console.log(sum);
+//         current = current.next;
+//     }
+//     return sum;
+// }
+
+
+// 2. recursive approach
+const sumList = (head) =>{
+    if (head == null) return 0;
+    return head.val += sumList(head.next);
+}
+
+console.log(sumList(a));
 
 //#endregion
 
