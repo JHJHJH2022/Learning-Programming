6d835fab91958021a0bb669b87b205abe1e2f96b
Jinghan0505 <jinghanjinghan233@gmail.com>
2022-12-16 14:17:04 +0800

    update

--- Projects/react-project-todolist/src/App.js
+++ Projects/react-project-todolist/src/App.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import { useState, useEffect } from "react"
 import Header from'./components/Header'
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () =>{
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks,setTasks] = useState([])
+
+  useEffect(()=>{
+    const getTasks = async()=>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+// Fetch Tasks
+const fetchTasks = async ()=>{
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+
+  return data
+}
+//Add Task
+const addTask = (task)=>{
+  const id = Math.floor(Math.random() * 10000) +1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+// Delete Task
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task)=>task.id!==id))
+}
+
+//toggle reminder
+const toggleReminder = (id)=>{
+  setTasks(tasks.map((task)=>task.id===id?{...task, reminder:!task.reminder}:task))
+}
+
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)}
+      showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>} 
+      {tasks.length>0 ? (
+      <Tasks tasks={tasks} 
+      onDelete={deleteTask} 
+      onToggle = {toggleReminder}/
+      >
+      ) : ("Add a task to start :D")}
     </div>
     
   );
